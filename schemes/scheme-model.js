3a4cd5ff2e41fd70a416a2fa190ec05c8196befc
Daniel Mattox <dmattox10@gmail.com>
2020-02-19 14:10:58 -0500

    Need to do queries

---
+++ schemes/scheme-model.js
@@ -0,0 +1,43 @@
+const db = require('../data/db-config')
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return  db('schemes')
+}
+
+function findById(id) {
+    return db('schemes')
+    .where({ id })
+    .first()
+}
+
+function findSteps(id) {
+    return db('schemes')
+    .join('steps', 'schemes.id', 'steps.scheme_id')
+    .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+}
+
+function add(scheme) {
+    return db('schemes')
+    .insert(scheme, 'id')
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where({ id })
+    .update(changes)
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({ id })
+    .del()
+}
